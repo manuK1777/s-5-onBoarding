c5e59b05d805bacb723ba099ec8e13258d24cb9f
manuK1777 <manuelkrapovickas@gmail.com>
2024-08-20 16:41:08 +0200

    Exercise 3 completed. Added styles to Escena component

--- src/app/services/steps.service.ts
+++ src/app/services/steps.service.ts
@@ -11,15 +11,21 @@ export class StepsService {
     return [  
       {
         title: 'Dedica moltes hores',
-        description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.'
+        description: 'Un mínim de 30 hores a la setmana. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.',
+        img: "assets/time_managment.svg",
+        bgcolor: '#3d9ba4'
       },
       {
         title: 'Programa projectes propis:',
-        description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.'
+        description: 'Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.',
+        img: "assets/programming.svg",
+        bgcolor: '#ceced5'
       },
       {
         title: 'Procura descansar:',
-        description: `Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.`
+        description: `Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.`,
+        img: "assets/meditation.svg",
+        bgcolor: '#fadd4d'
       }
     ]
   }
